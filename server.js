47cfa89d5c695df238c1d39d56e5df5b16c48e62
Dario <dario.braun@adesso.de>
2017-11-14 16:13:48 +0100

    save

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
+var readline = require('readline');
+var cp = require('child_process');
+var tail = cp.spawn('tail', ['-500', 'mylogfile.log']);
+var lineReader = readline.createInterface(tail.stdout, tail.stdin);
+
+lineReader.on('line', function(line) {
+	console.log('Line: ' + line);
+});
+
+tail.on('close', function(code, signal) {
+	console.log('ls finished...');
+});
+
 // API file for interacting with MongoDB
 const api = require('./server/routes/api');
 
 // Parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -19,7 +32,7 @@ app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
 //Set Port
